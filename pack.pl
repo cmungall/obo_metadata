633c5b6164080f148c6441645106b362836b136b
Chris Mungall <cjm@berkeleybop.org>
2017-10-27 19:09:12 -0700

    bump

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(obo_metadata).
 title('RDF vocabulary for OBO metadata ontologies').
-version('0.0.2').
+version('0.0.3').
 author('Chris Mungall','cjmungall@lbl.gov').
 home('https://github.com/cmungall/obo_metadata').
 download('https://github.com/cmungall/obo_metadata/releases/*.zip').
